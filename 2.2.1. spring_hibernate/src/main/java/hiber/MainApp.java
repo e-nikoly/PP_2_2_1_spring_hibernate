21fdf91be192be376fcb492db93a1f0cbe0cf280
e-nikoly <you@example.come-nikoly@bk.ru>
2022-09-11 12:31:35 +0700

    save1

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,24 +1,24 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
+   public static void main(String[] args) {
       AnnotationConfigApplicationContext context = 
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("audi", 100)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("bmv", 200)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("mercedes", 300)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("honda", 400)));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,8 +26,13 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("car_id = "+user.getCar().getId());
          System.out.println();
+
+
       }
+      User user = userService.getUserByCar("honda", 400);
+      System.out.println(user);
 
       context.close();
    }
